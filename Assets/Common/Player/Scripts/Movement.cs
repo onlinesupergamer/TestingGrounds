b75453aaaed1a22257b308643ab75bc5c486cdd6
onlinesupergamer <bennyguybills@gmail.com>
2023-02-25 04:41:13 -0700

    Small fixes

--- Assets/Common/Player/Scripts/Movement.cs
+++ Assets/Common/Player/Scripts/Movement.cs
@@ -12,21 +12,21 @@ using UnityEngine.EventSystems;
 */
 public class Movement : MonoBehaviour
 {
-    PlayerInput playerInput; //Ignore this for now, it's unused
+    PlayerInput playerInput; // Ignore this for now, it's unused
 
-    CharacterController characterController; //Gets controller
+    CharacterController characterController; // Gets controller
 
 
-    public float jumpSpeed = 5f; //Jump velocity
+    public float jumpSpeed = 5f; // Jump velocity
 
-    public float speed = 7f; //Player movement speed
+    public float speed = 7f; // Player movement speed
     public float rotationSpeed = 720f; // player rotation speed
     public float jumpButtonGracePeriod = 0.2f; // How forgiving the jumping is; ie, slightly too late off a ledge
 
     public float gravityAmount = 15f; // How much gravity to be applied
 
     [SerializeField]
-    Transform cameraTransform; //Gets main Camera Automatically; MAKE SURE THE PLAYER CAMERA IS TAGGED AS MAIN CAMERA
+    Transform cameraTransform; // Gets main Camera Automatically; MAKE SURE THE PLAYER CAMERA IS TAGGED AS MAIN CAMERA
 
     float ySpeed;
     float originalStepOffset;
@@ -39,23 +39,23 @@ public class Movement : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         originalStepOffset = characterController.stepOffset;
 
-        cameraTransform = Camera.main.transform; //Should Automatically be set if it's done correctly
+        cameraTransform = Camera.main.transform; // Should Automatically be set if it's done correctly
 
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");    //Gets the input value
+        float horizontalInput = Input.GetAxis("Horizontal");    // Gets the input value
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput); //Creates the movement value
-        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed; //Clamps the movement value
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput); // Creates the movement value
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed; // Clamps the movement value
         
         movementDirection.Normalize();
 
-        movementDirection = Quaternion.AngleAxis(cameraTransform.eulerAngles.y, Vector3.up) * movementDirection; //Makes the movement value relative to player camera
+        movementDirection = Quaternion.AngleAxis(cameraTransform.eulerAngles.y, Vector3.up) * movementDirection; // Makes the movement value relative to player camera
 
-        ySpeed += gravityAmount * Time.deltaTime; //Applies gravity to player at a rate
+        ySpeed += gravityAmount * Time.deltaTime; // Applies gravity to player at a rate
 
 
         ////////////////////////////////////////////////////////////////////////////////
@@ -93,21 +93,21 @@ public class Movement : MonoBehaviour
         ////////////////////////////////////////////////////////////////////////////////
 
         Vector3 Velocity = movementDirection * magnitude;
-        Velocity.y = ySpeed; //Adds gravity to movement calculation
+        Velocity.y = ySpeed; // Adds gravity to movement calculation
 
-        characterController.Move(Velocity * Time.deltaTime); //Actually moves the player
+        characterController.Move(Velocity * Time.deltaTime); // Actually moves the player
 
-        if (movementDirection != Vector3.zero) //This part just calculates player rotation
+        if (movementDirection != Vector3.zero) // This part just calculates player rotation
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime); //This does the actual rotation
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime); // This does the actual rotation
         }
     }
 
     private void OnApplicationFocus(bool focus)
     {
-        //Quality of life thing, locks the mouse when the application is in focus
+        // Quality of life thing, locks the mouse when the application is in focus
 
         if (focus)
         {
